9561326fdaaf4cb30b30a95ba0c49a6ecf59ed9c
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-08-17 18:23:50 +0200

    docs: introduce the TODO list concept

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -3,6 +3,10 @@ namespace TheMoneyExample.Tests;
 // We need to be able to add amounts in two different currencies and convert the result given a set of exchange rates.
 // We need to be able to multiply an amount (price per share) by a number (number of shares) and receive an amount.
 
+// TODO list
+// [] $5 + 10 CHF = $10 if rate is 2:1
+// [] $5 * 2 = $10
+
 public class Tests
 {
 }
