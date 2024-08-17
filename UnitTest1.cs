4017b6cf804b7b097698527e19863525fbc6764d
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-08-17 18:25:06 +0200

    test!: multiply a dollar amount by 2

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -9,4 +9,11 @@ namespace TheMoneyExample.Tests;
 
 public class Tests
 {
+    [Test]
+    public void TestMultiplication()
+    {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        Assert.AreEqual(10, five.amount);
+    }
 }
