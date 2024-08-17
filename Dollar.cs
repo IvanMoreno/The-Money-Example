d47ce32c91de6abfbc179e1a93892e0ded654b4c
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-08-17 19:15:38 +0200

    feat: multiply francs amounts
    We have temporarily duplicated the Dollar class, but we will remove this duplication in the refactoring phase

--- Dollar.cs
+++ Dollar.cs
@@ -19,3 +19,23 @@ public class Dollar
         return ((Dollar)obj).amount == amount;
     }
 }
+
+public class Franc
+{
+    int amount;
+
+    public Franc(int amount)
+    {
+        this.amount = amount;
+    }
+
+    public Franc Times(int multiplier)
+    {
+        return new Franc(amount * multiplier);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return ((Franc)obj).amount == amount;
+    }
+}
