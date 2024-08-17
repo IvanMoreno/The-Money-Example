032f31d945fd25f407495c71c6d32b6b2b4fb93c
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-08-17 18:40:24 +0200

    docs: cross dollar multiplication from the TODO list

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -5,7 +5,7 @@ namespace TheMoneyExample.Tests;
 
 // TODO list
 // [] $5 + 10 CHF = $10 if rate is 2:1
-// [] $5 * 2 = $10
+// [x] $5 * 2 = $10
 // [] Make "amount" private
 // [] Dollar side-effects?
 // [] Money rounding?
