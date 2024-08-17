b594585f5ceb8bb195e4c5aa2f9b53be58c158d2
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-08-17 18:45:01 +0200

    test!: change multiplication test to validate dollar immutability

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -16,7 +16,9 @@ public class Tests
     public void TestMultiplication()
     {
         Dollar five = new Dollar(5);
-        five.Times(2);
-        Assert.AreEqual(10, five.amount);
+        Dollar product = five.Times(2);
+        Assert.AreEqual(10, product.amount);
+        product = five.Times(3);
+        Assert.AreEqual(15, product.amount);
     }
 }
