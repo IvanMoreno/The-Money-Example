85ac02075b003ee338817d46d40d7b8ae683389e
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-08-17 18:20:11 +0200

    docs: document the requirements

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,5 +1,8 @@
 namespace TheMoneyExample.Tests;
 
+// We need to be able to add amounts in two different currencies and convert the result given a set of exchange rates.
+// We need to be able to multiply an amount (price per share) by a number (number of shares) and receive an amount.
+
 public class Tests
 {
 }
