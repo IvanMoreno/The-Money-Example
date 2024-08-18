1dbc4f0f8dbf79741af044b3eafed8c218643853
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-08-18 18:26:03 +0200

    refactor!: return new instance of franc using its factory method

--- Dollar.cs
+++ Dollar.cs
@@ -36,11 +36,11 @@ public class Franc : Money
     public Franc(int amount, string currency)
     {
         this.amount = amount;
-        currency = "CHF";
+        this.currency = "CHF";
     }
 
     public override Money Times(int multiplier)
     {
-        return new Franc(amount * multiplier);
+        return Money.Franc(amount * multiplier);
     }
 }
