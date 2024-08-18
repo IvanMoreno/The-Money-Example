1e755af964beae9c2d09439999b7a4f802f61303
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-08-18 18:00:52 +0200

    test: replace all franc constructors with factory method

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -37,16 +37,16 @@ public class Tests
     {
         Assert.AreEqual(Money.Dollar(5), Money.Dollar(5));
         Assert.AreNotEqual(Money.Dollar(5), Money.Dollar(6));
-        Assert.AreEqual(new Franc(5), new Franc(5));
-        Assert.AreNotEqual(new Franc(5), new Franc(6));
+        Assert.AreEqual(Money.Franc(5), Money.Franc(5));
+        Assert.AreNotEqual(Money.Franc(5), Money.Franc(6));
         Money.Franc(5).Should().NotBe(Money.Dollar(5));
     }
     
     [Test]
     public void TestFrancMultiplication()
     {
-        Franc five = new Franc(5);
-        Assert.AreEqual(new Franc(10), five.Times(2));
-        Assert.AreEqual(new Franc(15), five.Times(3));
+        Money five = Money.Franc(5);
+        Assert.AreEqual(Money.Franc(10), five.Times(2));
+        Assert.AreEqual(Money.Franc(15), five.Times(3));
     }
 }
