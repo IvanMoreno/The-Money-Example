846b25e73cc13c78f56638a86288427c2ee55f15
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-08-19 17:11:02 +0200

    refactor: delete dollar class

--- Dollar.cs
+++ Dollar.cs
@@ -33,12 +33,6 @@ public class Money
     public static Money Franc(int amount) => new Money(amount, "CHF");
 }
 
-public class Dollar : Money
-{
-    public Dollar(int amount, string currency)
-        : base(amount, currency){}
-}
-
 public class Franc : Money
 {
     public Franc(int amount, string currency)
