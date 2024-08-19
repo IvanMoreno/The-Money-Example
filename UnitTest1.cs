f9c82f9437ce27eaefa90e6323f7d0003f16a01f
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-08-19 17:34:25 +0200

    docs: return money from $5 + $5

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -8,6 +8,7 @@ namespace TheMoneyExample.Tests;
 // TODO list
 // [] $5 + 10 CHF = $10 if rate is 2:1
 // [] $5 + $5 = $10
+//      [] Return Money from $5 + $5
 // [] Money rounding?
 // [] Equal object
 
